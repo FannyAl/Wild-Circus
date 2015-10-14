e346d84bb6af2a1fe5b5c27e6a690d49b8d60184
FannyAl <fanny.alliaume@gmail.com>
2015-10-14 23:24:56 +0200

    Create footer_nav.php

---
+++ footer_nav.php
@@ -0,0 +1,19 @@
+# Wild-Circus
+Code source du site Wild Circus by the Peacocks Company
+
+<footer>
+
+	<nav>
+		<ul>
+			<li><a href="index.php">Program</a></li>
+			<li><a href="Performances.php">Performances</a></li>
+			<li><a href="About.php">About us</a></li>
+			<li><a href="Contact.php">Contact   </a></li>
+		</ul>
+	</nav>
+	
+				
+	<p>All rights reserved by Peacockspider(wo)man.</p>
+	<a href="https://www.expedia.com/Destinations-In-Australia.d10.Flight-Destinations" target="_blank" title="Come to see us in Australia!" class="bouton_ticketing"><img id="Ticketing" src="illustrations/Ticketing.png" alt="Ticketing" /></a>
+							
+</footer>
