69d7759ae255ad408af5c924b76ac51db9f7fa98
FannyAl <fanny.alliaume@gmail.com>
2015-10-14 23:24:22 +0200

    Create bottom.php

---
+++ bottom.php
@@ -0,0 +1,7 @@
+# Wild-Circus
+Code source du site Wild Circus by the Peacocks Company
+
+<!-- Bottom_page -->
+
+<a href="#" title="Top page"><img id="bas_page" src="illustrations/Eyes.png" alt="illustration" /></a>
+
