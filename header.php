0c3e4e77ac9ca2da8005df84182aab1dcb9a6cfa
FannyAl <fanny.alliaume@gmail.com>
2015-10-14 23:23:53 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,6 @@
+# Wild-Circus
+Code source du site Wild Circus by the Peacocks Company
+
+<header>
+	<a href="index.php"><center><img id="logo" src="illustrations/Wild-Circus.png" alt="The Wild Circus Logo"/></center></a>
+</header>
